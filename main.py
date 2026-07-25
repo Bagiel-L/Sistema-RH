892f43f176426e9cdf48c4327fb232d6abda774b
Gabriel Lima Rodrigues da Silva <gabriel224667@gmail.com>
2026-07-24 23:52:32 -0300

    Cria estrutura de dados do colaborador

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 print("=== Sistema de RH ===")
 
 nome = input("Digite o nome do colaborador:")
@@ -11,3 +13,16 @@ print("Setor adicionado", setor)
 
 salario = float(input("Digite o salário: ").replace(",", "."))
 print("Salário cadastrado:",salario)
+
+colaborador = {
+    "nome": nome,
+    "cargo": cargo,
+    "setor": setor,
+    "salario": salario
+}
+
+print("\n=== Colaborador Cadastrado ===")
+print("Nome:", colaborador["nome"])
+print("Cargo:", colaborador["cargo"])
+print("Setor:", colaborador["setor"])
+print("Salário:", colaborador["salario"])
